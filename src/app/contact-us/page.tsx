41af7521259b8b6e934636470e1baf2c0519adb5
Khawar Hanif Khokhar <88875448+KhawarKhokhar@users.noreply.github.com>
2026-08-11 22:58:30 +0500

    Contact Us

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -170,10 +170,25 @@ export default function ContactPage() {
                     name="marketingConsent"
                   />
 
-                 <span>
+                <span>
   I agree to receive communications by text message regarding offers and transactions from Maverick Realty LLC.
-You may opt out by replying STOP or ask for more information by replying HELP. Message frequency varies. Message and data rates may apply.
-You may review our Privacy Policy <a>https://www.maverickrealtyz.com/privacy-policy </a> to learn how your data is used.
+
+  <br />
+
+  You may opt out by replying STOP or ask for more information by replying HELP. Message frequency varies. Message and data rates may apply.
+
+  <br />
+
+  You may review our Privacy Policy{" "}
+  <a
+    href="https://www.maverickrealtyz.com/privacy-policy"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="underline text-blue-600 hover:text-blue-800"
+  >
+    https://www.maverickrealtyz.com/privacy-policy
+  </a>{" "}
+  to learn how your data is used.
 </span>
                 </label>
 
